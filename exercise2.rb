a03a2e231a876e793d544ffd48d6a887e1f80160
Sean <scasteli@hotmail.com>
2014-10-21 14:32:46 -0400

    2nd exercise

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+#tip calculation
+puts 55.0 * 0.15
+
+#addition of string and integer
+puts "five" + 4.to_s
+
+#interpolation
+result = 45628 * 7839
+puts "When doing the multiplication in the exercise 
+you get #{result}"
+
+#Expression value
+puts (true && false) || (false && true) || !(false && false)
+
+
